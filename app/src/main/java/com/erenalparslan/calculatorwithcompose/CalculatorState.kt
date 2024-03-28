a7566d416f76ba102f6dd29a675b58eea0a6af57
Eren <erenalparslan52@gmail.com>
2024-03-28 23:37:11 +0300

    project compleated

---
+++ app/src/main/java/com/erenalparslan/calculatorwithcompose/CalculatorState.kt
@@ -0,0 +1,8 @@
+package com.erenalparslan.calculatorwithcompose
+
+
+data class CalculatorState(
+    val number1: String = "",
+    val number2: String = "",
+    val operation: CalculatorOperation? = null
+)
